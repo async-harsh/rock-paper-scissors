2e6b5442431e950fc5e4e06bf38d0404dc092783
Harsh singh <99748759+async-harsh@users.noreply.github.com>
2023-12-28 21:19:45 +0530

    Create the function to output rock,paper,scissor randomlly

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+// This is where the computer choice func will pick either rock, paper or scissors.
 const choices = ["Rock", "Paper", "Scissor"];
+// To create a random index between 0 - 2 to output diff options.
+const randIndex = Math.floor(Math.random() * 3);
+// Stores the final random output
+let FinalChoice = choices[randIndex];
+// This is the function which gnerates random output for computer.
+function getComputerChoice(FinalChoice) {
+  //Outputs computer's coice/pick.
+  console.log(FinalChoice);
+}
